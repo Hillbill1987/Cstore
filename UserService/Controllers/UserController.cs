e0c728cd2acd922ff8e6598d04c4196c5ffca8cb
Graeme Hill <49688915+Hillbill1987@users.noreply.github.com>
2021-04-27 20:00:15 +0100

    User Service

---
+++ UserService/Controllers/UserController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database;
+using UserService.Database.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        DatabaseContext db;
+        public UserController()
+        {
+            db = new DatabaseContext();
+
+        }
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return db.users.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}", Name ="Get")]
+        public User Get(int id)
+        {
+            return db.users.Find(id);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] User model)
+        {
+            try
+            {
+                db.users.Add(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status201Created, model);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User model)
+        {
+            try
+            {
+                db.users.Update(model);
+                db.SaveChanges();
+                return StatusCode(StatusCodes.Status200OK, model);
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                User user = db.users.Find(id);
+                if (user is not null)
+                {
+                    db.users.Remove(user);
+                    db.SaveChanges();
+                    return StatusCode(StatusCodes.Status200OK);
+                }
+                return StatusCode(StatusCodes.Status404NotFound, id);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
---
+++ UserService/Migrations/20210325144642_initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserService.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(11)", maxLength: 11, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
